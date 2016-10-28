259337bc6c8199bd4087548a5121f46db20e3f7a
Alexander Demchenko <email.root@gmail.com>
2016-10-28 08:57:33 +0300

    2016-10-28

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+'use strict'
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const sourcemaps = require('gulp-sourcemaps');
+const gulpIf = require('gulp-if');
+const del = require('del');
+const autoprefixer = require('gulp-autoprefixer');
+const mincss = require("gulp-minify-css");
+const uglify = require("gulp-uglify");
+
+
+const  isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'dev';
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'prod'
+        }
+    });
+    browserSync.watch('prod/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('sass', function () {
+    return gulp.src('dev/sass/**/*.scss')
+        .pipe(gulpIf(isDevelopment, sourcemaps.init()))
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulpIf(isDevelopment, sourcemaps.write()))
+        .pipe(gulp.dest('dev/css'))
+});
+
+gulp.task('html', function () {
+    return gulp.src('dev/*.html')
+        .pipe(gulp.dest('prod'))
+})
+
+gulp.task('json', function () {
+    return gulp.src('dev/json/**/*.*')
+        .pipe(gulp.dest('prod/json'))
+})
+
+gulp.task('js', function () {
+    return gulp.src('dev/js/**/*.*')
+        .pipe(gulpIf(!isDevelopment,uglify()))
+        .pipe(gulp.dest('prod/js'))
+})
+
+gulp.task('img', function () {
+    return gulp.src('dev/img/**/*.*')
+        .pipe(gulp.dest('prod/img'))
+})
+
+gulp.task('css', function () {
+    return gulp.src('dev/css/**/*.*')
+        .pipe(gulpIf(!isDevelopment,mincss()))
+        .pipe(gulp.dest('prod/css'))
+})
+
+gulp.task('clean', function () {
+    return del('prod');
+});
+
+gulp.task('watch', function () {
+    gulp.watch('dev/sass/**/*.scss', gulp.series('sass'));
+    gulp.watch('dev/*.html', gulp.series('html'));
+    gulp.watch('dev/json/**/*.json', gulp.series('json'));
+    gulp.watch('dev/img/**/*.*', gulp.series('img'));
+    gulp.watch('dev/js/**/*.*', gulp.series('js'));
+    gulp.watch('dev/css/**/*.*', gulp.series('css'));
+});
+
+gulp.task('build', gulp.series('clean',
+    gulp.parallel('sass', 'html', 'json', 'img', 'js'),
+    gulp.parallel('css', 'browserSync', 'watch')));
+
+gulp.task('default', gulp.series('build'));
---
+++ prod/js/main.js
@@ -0,0 +1,5 @@
+/**
+ * Created by w7 on 27.10.2016.
+ */
+'use strict'
+var i = 2;
