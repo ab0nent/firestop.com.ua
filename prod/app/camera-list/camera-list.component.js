c20a2dbbf128caba8cf6a88e06ef610e9e3b3de6
Alexander Demchenko <email.root@gmail.com>
2016-11-01 15:02:46 +0200

    commit

--- prod/app/camera-list/camera-list.component.js
+++ prod/app/camera-list/camera-list.component.js
@@ -9,6 +9,26 @@ angular.
         function CameraListController(Camera) {
             this.cameras = Camera.query();
             this.orderProp = 'id';
+
+            var self = this;
+            self.filter = {};
+            self.filterByManufacturer = filterByManufacturer;
+            self.getManufacturer = getManufacturer;
+
+            function filterByManufacturer(camera) {
+                return self.filter[camera.Manufacturer] || noFilter(self.filter);
+            }
+
+            function getManufacturer() {
+                return (self.cameras || []).
+                map(function (camera) { return camera.Manufacturer; }).
+                filter(function (cat, idx, arr) { return arr.indexOf(cat) === idx; });
+            }
+            function noFilter(filterObj) {
+                return Object.
+                keys(filterObj).
+                every(function (key) { return !filterObj[key]; });
+            }
         }
     ]
 });
---
+++ prod/app/core/core.module.js
@@ -0,0 +1,4 @@
+/**
+ * Created by w7 on 01.11.2016.
+ */
+angular.module('core', ['core.camera']);
