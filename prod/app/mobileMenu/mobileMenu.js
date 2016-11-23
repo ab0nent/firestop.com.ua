e23832d5f4ff5f7e33357eca66b7ce83f9effe17
Alexander Demchenko <email.root@gmail.com>
2016-11-23 17:10:57 +0200

    commit

--- prod/app/mobileMenu/mobileMenu.js
+++ prod/app/mobileMenu/mobileMenu.js
@@ -6,15 +6,19 @@ angular
         this.mobileMenuClick = function (item) {
             switch(item){
                 case 'ГЛАВНАЯ':
+                    changeActiveMenuItem(0);
                     $location.path('/');
                     break;
                 case 'ОБОРУДОВАНИЕ':
+                    changeActiveMenuItem(1);
                     $location.path('/products');
                     break;
                 case 'КОНТАКТЫ':
+                    changeActiveMenuItem(2);
                     $location.path('/contacts');
                     break;
                 default:
+                    changeActiveMenuItem(0);
                     $location.path('/');
             }
         };
